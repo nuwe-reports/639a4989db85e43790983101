15cee05c22a43bec5fa40430dc286c0b997c71c6
estefaniaderosa <estefania.derosa@gmail.com>
2022-12-14 23:50:56 +0100

    lorem ipsum

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited by @qrppablo.
+          Edited by @estefaniaderosa.
         </p>
         <a
           className="App-link"
